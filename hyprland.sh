9b3cf0b1152af92f29ccfe6b0742601c29f82058
Hernan Reyes <hernan_rm@outlook.es>
2024-07-09 05:40:14 -0600

    feat: adds hyprland base installation

---
+++ hyprland.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo dnf -y install hyprland
+sudo dnf -y install hyprland-devel 
+
+sudo dnf install pipx
+pipx install waypaper
+sudo dnf -y install pamixer
+sudo dnf -y install fastfetch
