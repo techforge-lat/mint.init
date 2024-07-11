38c3c8ea4a25ccde4ef6bc1efec991a7fa946dc4
Hernan Reyes <hernan_rm@outlook.es>
2024-07-11 12:59:43 -0600

    feat: adds font installation

---
+++ install-font.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+declare -a fonts=(
+    JetBrainsMono
+)
+
+# Instalar unzip y fontconfig si no están instalados
+sudo dnf update
+sudo dnf -y install unzip fontconfig
+
+version='2.1.0'
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir"
+    rm "$zip_file"
+done
+
+find "$fonts_dir" -name '*Windows Compatible*' -delete
+
+fc-cache -fv
